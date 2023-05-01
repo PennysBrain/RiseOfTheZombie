3cbafcc8b5aabfb8e5acdf74aeb3915ce0bd0695
Olmer "Sylastial <olmercr@gmail.com>
2023-04-30 22:24:58 -0500

    Zombie Sound

--- Rise of the Zombie/Assets/Scripts/Enemy.cs
+++ Rise of the Zombie/Assets/Scripts/Enemy.cs
@@ -2,9 +2,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
+    private void Awake()
+    {
+        this.GetComponent<AudioSource>().pitch = Random.Range(-2.5f, 2.5f);
+    }
     private void OnMouseDown()
     {
         this.gameObject.SetActive(false);    
     }
+
+    void OnEnable()
+    {
+       // this.GetComponent<AudioSource>().pitch = Random.Range(-2.5f, 2.5f);
+        this.GetComponent<AudioSource>().Play();
+        //Debug.Log("PrintOnEnable: script was enabled");
+    }
+
 }
