5c93591f7943bbc7f02bd0858626ccfbfa420ef7
Olmer "Penny" Chavez-Ramirez <32857108+PennysBrain@users.noreply.github.com>
2023-06-02 02:16:50 -0500

    Animation Control and Tighter Controls

---
+++ Rise of the Zombie/Assets/ControlAnimationSystem.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAnimationSystem : MonoBehaviour
+{
+    public Animator animator;
+    [SerializeField] PlayerStats stats;
+    [SerializeField] WeapondSystem weaponds;//Needs optimized
+
+    private void Start()
+    {
+        if (!animator)
+        {
+            animator = this.GetComponent<Animator>();
+        }
+
+        if (!stats)
+        {
+            stats = this.GetComponent<PlayerStats>();
+        }
+    }
+
+    private void Update()
+    {
+        //Movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetBool("isMoving", true);
+            animator.SetFloat("moveForward", -1);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("isMoving", true);
+            animator.SetFloat("moveForward", 1);
+        }
+
+        //Switch Weaponds
+        if (Input.GetKeyUp(KeyCode.Q))//Dual gun
+        {
+            animator.SetFloat("weaponHoldster", 1);
+            animator.SetTrigger("weaponSwitch");
+            weaponds.currentWeaponIndex = 0;
+            weaponds.ChangeWeapon();
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))//Rifle
+        {
+            animator.SetFloat("weaponHoldster", 2);
+            animator.SetTrigger("weaponSwitch");
+            weaponds.currentWeaponIndex = 1;
+            weaponds.ChangeWeapon();
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("isMoving", false);
+            animator.SetFloat("moveForward", 0);
+        }
+        //Shooting
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("isShooting", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetBool("isShooting", false);
+        }
+
+        //Death
+        if (stats.healthPoints <= 0)
+        {
+            animator.SetBool("isDead", true);
+        }
+    }
+}
